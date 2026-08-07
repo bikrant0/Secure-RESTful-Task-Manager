424afa626b1cba42c239888b4ec4397cd94a6aca
Bikrant <shilpakarbikrant@gmail.com>
2026-08-07 22:20:58 +0545

    fix(backend): bypass CustomUserManager to safely save user with first_name and role

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,21 +23,33 @@ class TaskSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'created_at', 'user']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source = 'first_name', required = False)
+    # Map the 'name' from JS to Django's 'first_name'
+    name = serializers.CharField(source='first_name', required=False)
+
     class Meta:
         model = User
-        fields = ['id','name', 'email','password', 'role']
+        fields = ['id', 'name', 'email', 'password', 'role']
         
         extra_kwargs = {
-            'password' : {'write_only': True},
-            'email' : {'required': True},
-            'role': {'required':False}
+            'password': {'write_only': True},
+            'email': {'required': True},
+            'role': {'required': False} 
         }
         
     def create(self, validated_data):
-        return User.objects.create_user(
-            email = validated_data['email'],
-            password = validated_data['password'],
-            first_name = validated_data.get('first_name', ''),
-            role = validated_data('role', 'User'),
+        email = validated_data['email']
+        password = validated_data['password']
+        first_name = validated_data.get('first_name', '')
+        role = validated_data.get('role', 'JUNIOR') 
+
+        user = User(
+            email=email,
+            first_name=first_name,
+            role=role
         )
+        
+        user.set_password(password)
+        
+        user.save()
+        
+        return user
